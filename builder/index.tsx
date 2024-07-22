c9e693c982e3b187b44bc81b3eb11b19265a3c4c
Akhil Ramani <ramaniakhil094@gmail.com>
2024-07-22 23:26:16 +1000

    add export and load

--- builder/index.tsx
+++ builder/index.tsx
@@ -1,35 +1,29 @@
-"use client"
+"use client";
 
-import { Toolbox } from '../components/Toolbox';
-import { SettingsPanel } from '../components/SettingsPanel';
-import { Topbar } from '../components/Topbar';
-import { UserContainer } from '../components/user/Container';
-import { UserButton } from '../components/user/Button';
-import { UserText } from '../components/user/Text';
-import { UserCard } from '../components/user/Card';
+import { Toolbox } from "../components/Toolbox";
+import { SettingsPanel } from "../components/SettingsPanel";
+import { Topbar } from "../components/Topbar";
+import { UserContainer } from "../components/user/Container";
+import { UserButton } from "../components/user/Button";
+import { UserText } from "../components/user/Text";
+import { UserCard } from "../components/user/Card";
 import { Editor, Frame, Element } from "@craftjs/core";
-import { userComponents } from '@/components/user';
+import { userComponents } from "@/components/user";
 
 export default function Builder() {
   return (
     <Editor resolver={userComponents}>
-      <div className='h-full'>
+      <div className="h-full">
         <Topbar />
         <div className="flex w-full h-full">
           <Toolbox />
 
           <Frame>
             <Element is={UserContainer} padding={5} background="#eee" canvas>
-              <UserCard />
-              <UserButton>Button</UserButton>
-              <UserText text="Hi world!" fontSize='20px' />
               <Element is={UserContainer} padding={2} background="#999" canvas>
-                <UserText text="It's me again!" fontSize='20px' />
+                <UserText text="Title text" fontSize="20px" />
               </Element>
             </Element>
-            {/* <Container background="#eee">
-              <Card />
-            </Container> */}
           </Frame>
 
           <div className="bg-white rounded-lg shadow-md h-full">
--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch } from "@/components/ui/switch";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
 import { useEditor } from "@craftjs/core";
+import lz from "lzutf8";
+import copy from "copy-to-clipboard";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Input } from "./ui/input";
 
 export const Topbar: React.FC = () => {
   const { actions, query, enabled } = useEditor((state) => ({
     enabled: state.options.enabled,
   }));
+
+  const onCopyExportClick = () => {
+    const json = query.serialize();
+    copy(lz.encodeBase64(lz.compress(json)));
+    toast("State copied to clipboard");
+  };
+
+  const [loadTxt, setLoadTxt] = useState<string>();
+
+  const onLoadClick = () => {
+    const json = lz.decompress(lz.decodeBase64(loadTxt as string));
+    actions.deserialize(json);
+
+    toast("State loaded");
+  };
 
   return (
     <div className="px-4 py-4 border-b">
@@ -16,15 +36,33 @@ export const Topbar: React.FC = () => {
           <Switch id="enable-switch" checked={enabled} onCheckedChange={(value) => actions.setOptions((options) => (options.enabled = value))} />
           <Label htmlFor="enable-switch">Enable</Label>
         </div>
-        <Button
-          // variant="outline"
-          size="sm"
-          onClick={() => {
-            console.log(query.serialize());
-          }}
-        >
-          Serialize JSON to console
-        </Button>
+
+        <div className="flex gap-4">
+          <Button variant="secondary" size="sm" onClick={onCopyExportClick}>
+            Serialize JSON to console
+          </Button>
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="default" size="sm">
+                Open
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Load project</SheetTitle>
+                <SheetDescription>Load your previous project from exported data</SheetDescription>
+              </SheetHeader>
+              <div className="py-4 pt-8 flex gap-4">
+                <Input onChange={(e) => setLoadTxt(e.target.value)} />
+
+                <SheetClose asChild>
+                  <Button onClick={onLoadClick}>Load</Button>
+                </SheetClose>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </div>
   );
